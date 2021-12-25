89e150714f044e8d1d31de524a04274f51cc7591
widyaageng <ageng.setyatutuko@gmail.com>
2021-12-25 13:13:22 +0800

    Prep: node pkgs checked

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const routerapi = express.Router();
 
+// ---------------- custom middlewares ----------------
+//app logger
+function appLogger(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+// ---------------- end of custom middlewares ----------------
+
+// -------------------- utility methods --------------------
+// -------------------- end of utility methods --------------------
+
+
+
+// -------------------- apply middlewares --------------------
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(path = '/', middleWareFunction = appLogger);
+app.use(path = '/', middleWareFunction = bodyParser.urlencoded({ extended: true }));
+app.use(path = '/', middlewareFunction = bodyParser.json());
+app.use(path = '/api', routerapi);
+// -------------------- end of apply middlewares --------------------
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
@@ -14,6 +34,7 @@ app.get('/', function (req, res) {
 
 
 
+// ---------------- listener -------------------
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
