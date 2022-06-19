e9b1b71cd8b42c9df4a42082c4b8f7bef25af3c4
Widya Ageng Setya Tutuko <39331919+widyaageng@users.noreply.github.com>
2022-06-20 02:27:43 +0800

    Update number_guess.sh

--- public/number_guess.sh
+++ public/number_guess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 NUMBER=$((RANDOM%1000 + 1))
-echo $NUMBER
+# echo $NUMBER
 echo -e "Enter your username:"
 read USERNAME
 
@@ -11,7 +11,8 @@ then
   echo -e "Welcome, $USERNAME! It looks like this is your first time here."
 
   #register new user
-  USERNAME_INSERT_RESULT=$($PSQL "INSERT INTO users(username,best_guess) VALUES('$USERNAME', 999999);")
+  USERNAME_INSERT_RESULT=$($PSQL "INSERT INTO users(username,best_game) VALUES('$USERNAME', 999999);")
+  BESTGAME=999999
   if [[ $USERNAME_INSERT_RESULT == "INSERT 0 1" ]]
   then
     USERNAME_ACTUAL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME';")
@@ -23,13 +24,25 @@ else
 
   #get number of games
   GAMESPLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME';")
+  GAMECOUNT="games"
 
-  #get the bestgameguesses
-  BESTGAMEGUESS=$($PSQL "SELECT best_guess FROM users WHERE username='$USERNAME';")
+  #get the bestgame
+  BESTGAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME';")
+  BESTGAMECOUNT="guesses"
+
+  if [[ $GAMESPLAYED == 1 ]]
+  then
+    GAMECOUNT="game"
+  fi
+
+  if [[ $BESTGAME == 1 ]]
+  then
+    BESTGAMECOUNT="guess"
+  fi
+
+  echo -e "Welcome back, $USERNAME_ACTUAL! You have played $GAMESPLAYED $GAMECOUNT, and your best game took $BESTGAME $BESTGAMECOUNT."
 
   UPDATE_GAMESPLAYED=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE username='$USERNAME';")
-
-  echo -e "Welcome back, $USERNAME_ACTUAL! You have played $GAMESPLAYED games, and your best game took $BESTGAMEGUESS guesses."
 fi
 
 TRIALNUMBER=0
@@ -44,14 +57,14 @@ do
     TRIALNUMBER=$((TRIALNUMBER+1))
   fi
 
-  if [[ $USERGUESS =~ ^[0-9]+$ ]]
+  if [[ $USERGUESS =~ ^[1-9][0-9]*$ ]]
   then
-    if [[ $USERGUESS < $NUMBER ]]
+    if [[ $USERGUESS -lt $NUMBER ]]
     then
       echo -e "It's higher than that, guess again:"
       read USERGUESS
       TRIALNUMBER=$((TRIALNUMBER+1))
-    elif [[ $USERGUESS > $NUMBER ]]
+    elif [[ $USERGUESS -gt $NUMBER ]]
     then
       echo -e "It's lower than that, guess again:"
       read USERGUESS
@@ -59,15 +72,26 @@ do
     else
       :
     fi
+  elif [[ $USERGUESS == 0 ]]
+  then
+    echo -e "That is not an integer, guess again:"
+    read USERGUESS
+    TRIALNUMBER=$((TRIALNUMBER+1))
   else
     echo -e "That is not an integer, guess again:"
     read USERGUESS
     TRIALNUMBER=$((TRIALNUMBER+1))
   fi
 done
-echo -e "You guessed it in $TRIALNUMBER tries. The secret number was $NUMBER. Nice job!"
 
-if [[ $TRIALNUMBER < $BESTGAMEGUESS ]]
+if [[ $TRIALNUMBER -lt $BESTGAME ]]
 then
-  UPDATE_BESTGAMEGUESS=$($PSQL "UPDATE users SET best_guess=$TRIALNUMBER WHERE username=$USERNAME;")
+  UPDATE_BESTGAME=$($PSQL "UPDATE users SET best_game=$TRIALNUMBER WHERE username='$USERNAME';")
+fi
+
+if [[ $TRIALNUMBER -gt 1 ]]
+then
+  echo -e "You guessed it in $TRIALNUMBER tries. The secret number was $NUMBER. Nice job!"
+else
+  echo -e "You guessed it in $TRIALNUMBER try. The secret number was $NUMBER. Nice job!"
 fi
